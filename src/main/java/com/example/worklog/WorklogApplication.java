a7c651377d5859d47a749a6498213b80c106d6d1
kwonghyun <momky4@naver.com>
2023-11-10 11:57:50 +0900

    first commit

---
+++ src/main/java/com/example/worklog/WorklogApplication.java
@@ -0,0 +1,13 @@
+package com.example.worklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorklogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorklogApplication.class, args);
+    }
+
+}
