dd22ab6ae36ffea8aee303ce09874d5c6a3e48c1
kwonghyun <momky4@naver.com>
2023-11-14 07:23:27 +0900

    feat: @EnableJpaAuditing 추가

--- src/main/java/com/example/worklog/WorklogApplication.java
+++ src/main/java/com/example/worklog/WorklogApplication.java
@@ -2,8 +2,10 @@ package com.example.worklog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WorklogApplication {
 
     public static void main(String[] args) {
