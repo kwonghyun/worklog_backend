3f5d082335e964f3ee09e126527d8a07adfb860b
kwonghyun <momky4@naver.com>
2023-11-27 02:59:27 +0900

    chore: start.sh 복사본 jar 실행하도록 변경

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 BUILD_JAR="/home/ubuntu/worklog_backend/build/libs/worklog-0.0.1-SNAPSHOT.jar"
-
+JAR_FILE="/home/ubuntu/worklog_backend/spring.jar"
 #NOW=$(date +%c)
 START_LOG="/home/ubuntu/worklog_backend/start.log"
 #ERROR_LOG="/home/ubuntu/worklog_backend/err.log"
-
-nohup java -jar $BUILD_JAR >> START_LOG
+cp $BUILD_JAR $JAR_FILE
+nohup java -jar $JAR_FILE >> START_LOG
 
