27856c8aa0978aa7ffc2c1f67e9d0d5ad3734d27
kwonghyun <momky4@naver.com>
2023-11-27 03:18:14 +0900

    chore: start.sh 경로 수정

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILD_JAR="/home/ubuntu/worklog_backend/build/libs/worklog-0.0.1-SNAPSHOT.jar"
-JAR_FILE="/home/spring.jar"
+JAR_FILE="/home/ubuntu/spring.jar"
 #NOW=$(date +%c)
 START_LOG="/home/ubuntu/worklog_backend/start.log"
 #ERROR_LOG="/home/ubuntu/worklog_backend/err.log"
