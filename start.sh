8a8b529785e15aefadb0de630f7484ce8de1fe16
kwonghyun <momky4@naver.com>
2023-11-27 02:16:16 +0900

    chore: start.sh 수정해서 jar파일 이름 그대로 실행하게

--- start.sh
+++ start.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-BUILD_JAR="/home/ubuntu/worklog_backend/build/libs/*.jar"
-JAR_NAME=$(basename $BUILD_JAR)
+BUILD_JAR="/home/ubuntu/worklog_backend/build/libs/worklog-0.0.1-SNAPSHOT.jar"
+
 
 NOW=$(date +%c)
 START_LOG="/home/ubuntu/worklog_backend/start.log"
 ERROR_LOG="/home/ubuntu/worklog_backend/err.log"
 
-echo "[$NOW] > build 파일명: $JAR_NAME" >> START_LOG
-
 echo "[$NOW] > build 파일 복사" >> START_LOG
-DEPLOY_PATH=/home/ubuntu/worklog_backend/
-cp $BUILD_JAR $DEPLOY_PATH
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-nohup java -jar $DEPLOY_JAR >> START_LOG 2>ERROR_LOG &
-SERVICE_PID=$(pgrep -f $JAR_NAME)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+nohup java -jar $BUILD_JAR >> START_LOG 2>ERROR_LOG &
+
