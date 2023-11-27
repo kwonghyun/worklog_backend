f35d1d74b7c6238b02c20fa7a270564f97852292
kwonghyun <momky4@naver.com>
2023-11-28 00:16:51 +0900

    feat: db 설정 로컬/배포 분리 - yaml 파일 분리 - gradle.yml 환경변수 할당 경로 수정 - build.gradle sqlite 제거 - start.sh 프로필 지정해서 jar파일 실행

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 BUILD_JAR="/home/ubuntu/worklog_backend/build/libs/worklog-0.0.1-SNAPSHOT.jar"
-JAR_FILE="/home/ubuntu/spring.jar"
-#NOW=$(date +%c)
-START_LOG="/home/ubuntu/worklog_backend/start.log"
-#ERROR_LOG="/home/ubuntu/worklog_backend/err.log"
-cp $BUILD_JAR $JAR_FILE
-cd /home/ubuntu
-nohup java -jar spring.jar > /home/ubuntu/worklog_backend/nohup.out 2>&1 &
+
+
+
+nohup java -jar -Dspring.profiles.active=prod $BUILD_JAR > /home/ubuntu/worklog_backend/nohup.out 2>&1 &
 
