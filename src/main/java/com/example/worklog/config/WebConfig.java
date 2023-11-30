c37d7b17507739307f4b2e7370bf09fb898e82ab
kwonghyun <momky4@naver.com>
2023-12-01 00:00:59 +0900

    fix: cors 전체 허용

--- src/main/java/com/example/worklog/config/WebConfig.java
+++ src/main/java/com/example/worklog/config/WebConfig.java
@@ -1,15 +1,12 @@
 package com.example.worklog.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:8100",
-                        "https://today-worklog.vercel.app"
-                );
+        registry.addMapping("/**");
     }
 }
