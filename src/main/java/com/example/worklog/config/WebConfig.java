d7189943afa6f79abe33dabf0742fb48137c0404
kwonghyun <momky4@naver.com>
2023-12-09 20:55:18 +0900

    update: cors 허용 주소 재설정 -주소 끝에 /붙여봄

--- src/main/java/com/example/worklog/config/WebConfig.java
+++ src/main/java/com/example/worklog/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("**")
                 .allowedOrigins(
                         "http://localhost:8100/",
                         "https://today-worklog.vercel.app/"
