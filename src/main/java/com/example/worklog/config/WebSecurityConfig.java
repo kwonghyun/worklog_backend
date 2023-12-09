d1c69ae419ad9270fa75e71a38848e66ccdbd039
kwonghyun <momky4@naver.com>
2023-12-09 21:12:21 +0900

    update: cors 허용 설정 WebSecurityConfig에 몰아서

--- src/main/java/com/example/worklog/config/WebSecurityConfig.java
+++ src/main/java/com/example/worklog/config/WebSecurityConfig.java
@@ -1,25 +1,28 @@
 package com.example.worklog.config;
 
 import com.example.worklog.jwt.JwtTokenFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
     private final JwtTokenFilter jwtTokenFilter;
-    private final CorsConfig corsConfig;
 
     @Bean
     public WebSecurityCustomizer configure() {
@@ -32,7 +35,18 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors((Customizer<CorsConfigurer<HttpSecurity>>) corsConfig)
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Arrays.asList("http://localhost:8100", "https://today-worklog.vercel.app"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L); //1시간
+                        return config;
+                    }
+                }))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authHttp -> authHttp
                         .requestMatchers(
