fd13c0b28c7ec2dab70fcef7dd57a4a9e3f0589c
kwonghyun <momky4@naver.com>
2024-01-23 17:18:34 +0900

    chore: sse mediatype 원상 복구 이거랑 메세지 수신 안되는 거랑 상관 없었음

--- src/main/java/com/example/worklog/service/SseService.java
+++ src/main/java/com/example/worklog/service/SseService.java
@@ -51,7 +51,7 @@ public class SseService {
                             .name("connect")
                             .reconnectTime(sseTimeout)
                             .data("connect")
-                            .build(), MediaType.TEXT_EVENT_STREAM
+                            .build()
             );
             log.info("username: {} 에게 sse 연결 성공", key.split("_")[0]);
         } catch (IOException exception) {
@@ -75,7 +75,7 @@ public class SseService {
                             .name(emitterKey.split("_")[1])
                             .reconnectTime(sseTimeout)
                             .data(dto, MediaType.APPLICATION_JSON)
-                            .build(), MediaType.TEXT_EVENT_STREAM
+                            .build()
             );
             log.info("SSE : username: {} 에게 sse message : {} 전송", emitterKey.split("_")[0], dto.toString());
         } catch (IOException exception) {
