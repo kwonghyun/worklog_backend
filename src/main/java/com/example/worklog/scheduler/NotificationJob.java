a230b96074f6008156d408c66dd8c08677de115b
kwonghyun <momky4@naver.com>
2024-01-24 17:48:59 +0900

    chore: 필요없어진 코드 제거

--- src/main/java/com/example/worklog/scheduler/NotificationJob.java
+++ src/main/java/com/example/worklog/scheduler/NotificationJob.java
@@ -3,7 +3,9 @@ package com.example.worklog.scheduler;
 import com.example.worklog.entity.Notification;
 import com.example.worklog.entity.Work;
 import com.example.worklog.entity.enums.NotificationEntityType;
+import com.example.worklog.entity.enums.SseRole;
 import com.example.worklog.service.NotificationService;
+import com.example.worklog.service.SseService;
 import com.example.worklog.service.WorkService;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobDataMap;
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Component;
 public class NotificationJob extends QuartzJobBean {
 
     private NotificationService notificationService;
+    private SseService sseService;
     private WorkService workService;
 
     @Override
@@ -26,6 +29,7 @@ public class NotificationJob extends QuartzJobBean {
         ApplicationContext appCtx = (ApplicationContext)context.getJobDetail()
                 .getJobDataMap().get("applicationContext");
         notificationService = appCtx.getBean(NotificationService.class);
+        sseService = appCtx.getBean(SseService.class);
         workService = appCtx.getBean(WorkService.class);
 
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
@@ -33,17 +37,19 @@ public class NotificationJob extends QuartzJobBean {
         Long workId = dataMap.getLong("workId");
         Long notificationId = dataMap.getLong("notificationId");
         String username = dataMap.getString("username");
-        if (workId == -1L && notificationId == -1L) {
-            System.out.println("초기화 Job 실행");
-        } else {
-            Work work = workService.findOne(workId);
-            Notification notification = notificationService.findOne(notificationId);
 
-            notification.setMessage(
-                    notificationService.generateMessage(NotificationEntityType.WORK, work.getId())
-            );
-
-            notificationService.sendNotification(username, notification);
+        if (!sseService.isSseConnected(username, SseRole.NOTIFICATION)) {
+            return;
         }
+
+        Work work = workService.findOne(workId);
+        Notification notification = notificationService.findOne(notificationId);
+
+        notification.setMessage(
+                notificationService.generateMessage(NotificationEntityType.WORK, work.getId())
+        );
+
+        notificationService.sendNotification(username, notification);
+
     }
 }
