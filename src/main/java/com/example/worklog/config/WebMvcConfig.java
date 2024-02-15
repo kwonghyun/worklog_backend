197e52f37fcdc11812ee682b2f3e222d262ea446
kwonghyun <momky4@naver.com>
2024-02-15 18:29:46 +0900

    chore: 로그 삭제

--- src/main/java/com/example/worklog/config/WebMvcConfig.java
+++ src/main/java/com/example/worklog/config/WebMvcConfig.java
@@ -45,29 +45,24 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 // 파라미터의 키 값을 snake_case에서 camelCase 변환 후 맵에 값을 가지고 있음
                 for(String param : request.getParameterMap().keySet()){
                     String camelCaseParam = StringConverter.convertSnakeToCamel(param);
-                    log.info(camelCaseParam);
                     paramters.put(camelCaseParam, request.getParameterValues(param));
                     paramters.put(param, request.getParameterValues(param));
                 }
 
                 // 필터체인을 이용하여, request에 해당 값을 추가하여 반환
                 filterChain.doFilter(new HttpServletRequestWrapper(request){
-
                     @Override
                     public String getParameter(String name){
                         return paramters.containsKey(name) ? paramters.get(name)[0] : null;
                     }
-
                     @Override
                     public Enumeration<String> getParameterNames(){
                         return Collections.enumeration(paramters.keySet());
                     }
-
                     @Override
                     public String[] getParameterValues(String name){
                         return paramters.get(name);
                     }
-
                     @Override
                     public Map<String, String[]> getParameterMap(){
                         return paramters;
