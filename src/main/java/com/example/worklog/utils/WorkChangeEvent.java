4ae0017ed3bf9551d78ea953bddf79adefaebd8e
kwonghyun <momky4@naver.com>
2024-02-16 18:18:19 +0900

    chore: toString추가

--- src/main/java/com/example/worklog/utils/WorkChangeEvent.java
+++ src/main/java/com/example/worklog/utils/WorkChangeEvent.java
@@ -3,9 +3,11 @@ package com.example.worklog.utils;
 import com.example.worklog.entity.Work;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder
+@ToString
 public class WorkChangeEvent {
     private Work work;
 }
