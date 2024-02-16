92d3725f72cacb8a224bb05a4e2f38b219e929c3
kwonghyun <momky4@naver.com>
2024-02-16 10:25:48 +0900

    refactor: 알림 알릴 시간 인지 여부는 UserService 내부로 숨겨 클라이언트가 몰라도 되도록 endpoint 삭제

--- src/main/java/com/example/worklog/controller/NotificationController.java
+++ src/main/java/com/example/worklog/controller/NotificationController.java
@@ -1,6 +1,5 @@
 package com.example.worklog.controller;
 
-import com.example.worklog.dto.ResourceResponseDto;
 import com.example.worklog.dto.ResponseDto;
 import com.example.worklog.dto.user.CustomUserDetails;
 import com.example.worklog.exception.SuccessCode;
@@ -10,12 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,12 +25,5 @@ public class NotificationController {
         notificationService.checkNotificationAndSend(userDetails.getId());
 
         return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.fromSuccessCode(SuccessCode.SUCCESS));
-    }
-
-    @GetMapping("/noticed-at/check")
-    public ResponseEntity<ResourceResponseDto> check(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        Boolean isTimeToNotice = notificationService.isTimeToNotice(userDetails.getLastNoticedAt());
-        return ResponseEntity.status(HttpStatus.OK).body(ResourceResponseDto.fromData(
-                new HashMap<>(){{put("isTimeToNotice", isTimeToNotice);}}, 1));
     }
 }
