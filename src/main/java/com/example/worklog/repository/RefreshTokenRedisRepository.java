c852e4a8695e9c7f9ef5d3bf5180ebc4c5724ce4
kwonghyun <momky4@naver.com>
2024-02-18 00:07:56 +0900

    chore: 클래스 이름 변경

--- src/main/java/com/example/worklog/repository/RefreshTokenRedisRepository.java
+++ src/main/java/com/example/worklog/repository/RefreshTokenRedisRepository.java
@@ -1,12 +1,12 @@
 package com.example.worklog.repository;
 
-import com.example.worklog.entity.RefreshToken;
+import com.example.worklog.entity.RefreshTokenDetails;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String> {
-    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+public interface RefreshTokenRedisRepository extends CrudRepository<RefreshTokenDetails, String> {
+    Optional<RefreshTokenDetails> findByRefreshToken(String refreshToken);
     boolean existsById(String username);
     void deleteById(String username);
 }
