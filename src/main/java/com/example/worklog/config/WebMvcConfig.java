48959ef6400f5570ecd36a54a1e7ab6be8af3905
kwonghyun <momky4@naver.com>
2024-02-18 00:03:25 +0900

    chore: 필요 없어져서 지움

--- src/main/java/com/example/worklog/config/WebMvcConfig.java
+++ src/main/java/com/example/worklog/config/WebMvcConfig.java
@@ -31,47 +31,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(requestLoggingInterceptor)
                 .excludePathPatterns("/css/**", "/images/**", "/js/**");
     }
-
-    @Bean // 필터를 빈 선언
-    public OncePerRequestFilter snakeCaseConverterFilter(){
-        return new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-                    throws ServletException, IOException
-            {
-
-                final Map<String, String[]> paramters = new ConcurrentHashMap<>();
-
-                // 파라미터의 키 값을 snake_case에서 camelCase 변환 후 맵에 값을 가지고 있음
-                for(String param : request.getParameterMap().keySet()){
-                    String camelCaseParam = StringConverter.convertSnakeToCamel(param);
-                    paramters.put(camelCaseParam, request.getParameterValues(param));
-                    paramters.put(param, request.getParameterValues(param));
-                }
-
-                // 필터체인을 이용하여, request에 해당 값을 추가하여 반환
-                filterChain.doFilter(new HttpServletRequestWrapper(request){
-                    @Override
-                    public String getParameter(String name){
-                        return paramters.containsKey(name) ? paramters.get(name)[0] : null;
-                    }
-                    @Override
-                    public Enumeration<String> getParameterNames(){
-                        return Collections.enumeration(paramters.keySet());
-                    }
-                    @Override
-                    public String[] getParameterValues(String name){
-                        return paramters.get(name);
-                    }
-                    @Override
-                    public Map<String, String[]> getParameterMap(){
-                        return paramters;
-                    }
-
-                }, response);
-            }
-
-        };
-    }
-
 }
