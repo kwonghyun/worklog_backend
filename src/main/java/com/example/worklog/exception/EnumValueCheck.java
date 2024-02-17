d59779062240415df718f395154419e132335d62
kwonghyun <momky4@naver.com>
2024-02-17 23:06:46 +0900

    feat: 요청에서 Enum값 validation 어노테이션 생성 - Dto에서 바로 enum으로 받으면 유효성 검사하기 전에 역직렬화할때 예외 발생해서 불가능 - String 값으로 받도록

---
+++ src/main/java/com/example/worklog/exception/EnumValueCheck.java
@@ -0,0 +1,17 @@
+package com.example.worklog.exception;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = ValueOfEnumValidator.class)
+public @interface EnumValueCheck {
+    Class<? extends Enum<?>> enumClass();
+    String message() default "";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/example/worklog/exception/ValueOfEnumValidator.java
@@ -0,0 +1,34 @@
+package com.example.worklog.exception;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ValueOfEnumValidator implements ConstraintValidator<EnumValueCheck, String> {
+
+    private static final Logger log = LoggerFactory.getLogger(EnumValueCheck.class);
+    private EnumValueCheck enumValueCheck;
+    @Override
+    public void initialize(EnumValueCheck constraintAnnotation) {
+        this.enumValueCheck = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+        try {
+            Method fromMethod = this.enumValueCheck.enumClass().getMethod("from", String.class);
+            fromMethod.invoke(null, value);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            log.error("Error validating enum value: {}", e.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
