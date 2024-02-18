f68db937676f725d3b25475cf9659d1e5a91ddfe
kwonghyun <momky4@naver.com>
2024-02-18 14:12:11 +0900

    chore: 알림 예약 시간 2시간으로 변경

--- src/main/java/com/example/worklog/utils/EnvironmentVariable.java
+++ src/main/java/com/example/worklog/utils/EnvironmentVariable.java
@@ -2,5 +2,5 @@ package com.example.worklog.utils;
 
 public class EnvironmentVariable {
     public static final long WORK_DEADLINE_TRIGGER_HOURS = 24L;
-    public static final long SEARCH_FUTURE_NOTIFICATION_MINUTES = 60L;
+    public static final long SEARCH_FUTURE_NOTIFICATION_MINUTES = 120L;
 }
