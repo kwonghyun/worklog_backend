bb7ba27245be4dd5ba557acb08c21ebee5998ca0
kwonghyun <momky4@naver.com>
2024-03-06 13:09:20 +0900

    chore: 유효성 검사 어노테이션 추가

--- src/main/java/com/example/worklog/dto/work/WorkCategoryPatchDto.java
+++ src/main/java/com/example/worklog/dto/work/WorkCategoryPatchDto.java
@@ -1,5 +1,7 @@
 package com.example.worklog.dto.work;
 
+import com.example.worklog.entity.enums.Category;
+import com.example.worklog.exception.validation.EnumValueCheck;
 import com.example.worklog.utils.Constants;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -9,5 +11,6 @@ import lombok.ToString;
 @ToString
 public class WorkCategoryPatchDto {
     @NotNull(message = Constants.CATEGORY_NOT_BLANK)
+    @EnumValueCheck(enumClass = Category.class)
     private String category;
 }
