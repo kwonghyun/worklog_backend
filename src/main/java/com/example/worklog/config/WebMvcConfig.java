8aa88210d532108e85ddc2ad77ed1571890910bb
gwang <momky4@naver.com>
2025-01-13 00:07:58 +0900

    chore: 임포트문 최적화

--- src/main/java/com/example/worklog/config/WebMvcConfig.java
+++ src/main/java/com/example/worklog/config/WebMvcConfig.java
@@ -1,25 +1,11 @@
 package com.example.worklog.config;
 
 import com.example.worklog.aop.logging.RequestLoggingInterceptor;
-import com.example.worklog.utils.StringConverter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletRequestWrapper;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 @RequiredArgsConstructor
